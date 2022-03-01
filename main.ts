f5840853e38d17811556000f85acf92acfc4e84a
NotTracy <100299623+NotTracy@users.noreply.github.com>
2022-03-01 11:11:42 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onGesture(Gesture.LogoUp, function () {
+    sprite.change(LedSpriteProperty.Y, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    sprite.change(LedSpriteProperty.X, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    sprite.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    sprite.change(LedSpriteProperty.Y, -1)
+})
+let sprite: game.LedSprite = null
+basic.showIcon(IconNames.Duck)
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+	
+})
